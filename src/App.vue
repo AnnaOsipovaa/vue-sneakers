739f14996d3f9b8dc985b395afeff1d4a13f8fab
Anna Osipova <a.osipova.a@yandex.ru>
2024-08-25 20:00:46 +0300

    инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import MyComponent from './components/MyComponent.vue';
+
+</script>
+
+<template>
+  <div class="mt-40">
+    <MyComponent title="Кросовки" :price="9500"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    title: String,
+    price: Number
+});
+</script>
+
+<template>
+  <div class="block">
+    <div>Имя {{ title }}</div>
+    <div>Информация прайс {{ price }}</div>
+  </div>
+</template>
+
+
+
