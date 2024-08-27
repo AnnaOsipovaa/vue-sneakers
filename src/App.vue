d077c248e122b5b804b7d479d88039170f7a8905
Anna Osipova <a.osipova.a@yandex.ru>
2024-08-27 20:46:20 +0300

    доделала header, вынесла его в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
 
-import MyComponent from './components/MyComponent.vue';
+import Header from './components/Header.vue';
 
 </script>
 
 <template>
-  <div class="mt-40">
-    <MyComponent title="Кросовки" :price="9500"/>
-  </div>
+  <Header />
 </template>
 
 <style scoped>
