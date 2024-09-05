23adbfd095adbe41625f07dd90df2fcd1127b0ef
Anna Osipova <a.osipova.a@yandex.ru>
2024-09-05 21:28:46 +0300

    карточка и список товаров готовы

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+defineProps({
+    imageUrl: String,
+    title: String,
+    price: Number,
+    isLike: Boolean,
+    isCart: Boolean,
+    setIsLike: Function,
+    setIsCart: Function
+});
+
+</script>
+
+<template>
+    <div class="flex flex-col items-stretch box-border cursor-pointer pt-5 px-8 pb-8 relative bg-white max-w-52 rounded-3xl border border-slate-200 hover:-translate-y-2 hover:shadow-lg duration-300">
+        <div class="absolute top-7 left-7 hover:scale-110 duration-200">
+            <img :src="isLike ? '/like-2.svg' : '/like-1.svg'" v-on:click="setIsLike" alt="добавить в избранное">
+        </div>
+        <div class="w-32 h-28 mb-3.5">
+            <img class="max-w-full" :src="imageUrl" alt="">
+        </div>
+        <div class="text-sm text-black mb-3.5">{{title}}</div>
+        <div class="flex justify-between">
+        <div class="flex flex-col">
+            <div class="text-xs text-slate-400">Цена:</div>
+            <div class="text-sm font-bold">{{price}} руб.</div>
+        </div>
+        <div class="hover:scale-110 duration-200">
+            <img :src="isCart ? '/checked.svg' : '/plus.svg'" v-on:click="setIsCart" alt="добавить в корзину">
+        </div>
+        </div>
+    </div>
+  </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,36 @@
 <template>
-  <div class="w-4/5 mx-auto bg-white rounded-md shadow-xl mt-14">
-    <header class="px-8 py-7 flex justify-between items-center">
-      <div class="flex">
-        <div class="max-w-10 mr-4">
-          <img src="/logo.png" alt="logo" class="w-100">
-        </div>
-        <div>
-          <div class="text-black font-bold leading-6 text-xl">REACT SNEAKERS</div>
-          <div class="text-neutral-400 font-normal leading-4">Магазин лучших кроссовок</div>
-        </div>
-      </div >
-
-      <div class="flex items-center">
-        <div class="flex mr-8 items-center cursor-pointer hover:scale-110 duration-200">
-          <div class="mr-2.5">
-            <img src="/cart.svg" alt="корзина">
-          </div>
-          <div class="text-neutral-500 font-semibold text-sm">1205 руб.</div>
-        </div>
-        <div class="flex mr-8 items-center cursor-pointer hover:scale-110 duration-200">
-          <div class="mr-2.5">
-            <img src="/heart.svg" alt="закладки">
-          </div>
-          <div class="text-neutral-600 font-normal text-sm">Закладки</div>
-        </div>
-        <div class="flex items-center cursor-pointer hover:scale-110 duration-200">
-          <div class="mr-2.5">
-            <img src="/profile.svg" alt="профиль">
-          </div>
-          <div class="text-neutral-600 font-normal text-sm">Профиль</div>
-        </div>
+  <header class="px-8 py-7 flex justify-between items-center">
+    <div class="flex">
+      <div class="max-w-10 mr-4">
+        <img src="/logo.png" alt="logo" class="w-100">
       </div>
-    </header>
-  </div>
+      <div>
+        <div class="text-black font-bold leading-6 text-xl">REACT SNEAKERS</div>
+        <div class="text-neutral-400 font-normal leading-4">Магазин лучших кроссовок</div>
+      </div>
+    </div >
+
+    <div class="flex items-center">
+      <div class="flex mr-8 items-center cursor-pointer hover:scale-110 duration-200">
+        <div class="mr-2.5">
+          <img src="/cart.svg" alt="корзина">
+        </div>
+        <div class="text-neutral-500 font-semibold text-sm">1205 руб.</div>
+      </div>
+      <div class="flex mr-8 items-center cursor-pointer hover:scale-110 duration-200">
+        <div class="mr-2.5">
+          <img src="/heart.svg" alt="закладки">
+        </div>
+        <div class="text-neutral-600 font-normal text-sm">Закладки</div>
+      </div>
+      <div class="flex items-center cursor-pointer hover:scale-110 duration-200">
+        <div class="mr-2.5">
+          <img src="/profile.svg" alt="профиль">
+        </div>
+        <div class="text-neutral-600 font-normal text-sm">Профиль</div>
+      </div>
+    </div>
+  </header>
 </template>
 
 
