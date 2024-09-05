8754d5784574a20866a32fc2ada2718375c4b2be
Anna Osipova <a.osipova.a@yandex.ru>
2024-09-05 21:28:46 +0300

    карточка и список товаров готовы

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 
 import Header from './components/Header.vue';
-
+import CardList from './components/CardList.vue';
 </script>
 
 <template>
-  <Header />
+  <div class="w-4/5 mx-auto bg-white rounded-md shadow-xl mt-14">
+    <Header />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
+      <CardList />
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import Card from './Card.vue';
+
+function setIsLike(){
+    alert('setIsLike');
+}
+function setIsCart(){
+    alert('setIsCart');
+}
+</script>
+
+<template>
+      <div class="grid grid-cols-4 gap-5">
+        <Card 
+            :is-like="true" 
+            :is-cart="true" 
+            imageUrl="../../public/sneakers/sneakers-1.jpg" 
+            title="12345" 
+            :price="1223" 
+            :set-is-like="setIsLike"
+            :set-is-cart="setIsCart" />
+      </div>
+</template>  
